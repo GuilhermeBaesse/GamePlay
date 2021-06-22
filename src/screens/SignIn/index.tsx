b7c108ed194e7fab3fefecfbcfea85274de95e18
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-22 10:29:43 -0300

    corrigindo styles e index

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,35 +1,49 @@
-/* estrutura da interface*/
-import React, {useState} from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
-import illustrationImg from '../../assets/illustrationImg.png'
+import React from 'react';
+import { 
+  View, 
+  Text, 
+  Image,  
+  StatusBar,
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+import { ButtonIcon } from '../../componets/Buttonicon';
 
 export function SignIn(){
-const [text, setText] = useState('')
+  return(
+    <View style={styles.container}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image} 
+        resizeMode="stretch"
+      />
 
-    return(
-        <View style={styles.container}> 
-        
-            <StatusBar barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-            />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`} 
+          facilmente
+        </Text>
 
-            <Image
-                source={illustrationImg}
-                style={styles.image}
-                resizeMode="stretch"
-            />
-            <View style={styles.content}>
-                <Text style={styles.title}/>
-                    Organize{'\n'} suas jogatinas{'\n'} facilmente
-                <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games {'\n'}
-                    favoritos com seus amigos
-                </Text>
-            </View>
-        </View>
-    );
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`} 
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+        />                
+               
+      </View>
+    </View>
+  );
 }
-
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,31 +1,32 @@
-/*regras de estilização da interface*/
-import { StyleSheet, TouchableHighlightComponent } from "react-native";
-import { theme } from '../../assets/discord.png'
+import { StyleSheet } from 'react-native';
+
+import { theme } from '../../global/styles/theme';
+
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: theme.colors.background
-    },
-    image: {
-        width: '100%',
-        height: 360
-    },
-    content: {
-        marginTop: -40,
-        paddingHorizontal: 50,
-    },
-    title: {
-        color: theme.color.heading,
-        textAlign: 'center',
-        fontSize: 40,
-        marginBottom: 16, 
-    },
-    subtitle: {
-        color: theme.color.heading,
-        fontSize: 15,
-        textAlign: 'center',
-        marginBottom: 64
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.background
+  },
+  image: {
+    width: '100%',
+    height: 360,    
+  },
+  content: {    
+    marginTop: -40,    
+    paddingHorizontal: 50
+  },
+  title: {
+    color: theme.colors.heading,
+    textAlign: 'center',
+    fontSize: 40,
+    marginBottom: 16,    
+  },
+  subtitle: {
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: 'center',
+    marginBottom: 64
+  }
+});
