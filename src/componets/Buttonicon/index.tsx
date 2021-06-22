52572be7f04c7eccaff3c638ce9784d6bf0d330f
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-22 10:28:49 -0300

    corrigindo erros no styles e bugs

--- src/componets/Buttonicon/index.tsx
+++ src/componets/Buttonicon/index.tsx
@@ -1,33 +1,32 @@
-import React from "react";
-
+import React from 'react';
 import {
-    Text,
-    Image,
-    View,
-    TouchableOpacity,
-    TouchableOpacityProps,
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
 } from 'react-native';
 
-import DiscordImg from '../../componets/illustration.png';
+import DiscordImg from '../../assets/discord.png';
 import { styles } from './styles';
 
 type Props = TouchableOpacityProps & {
-    title: string;
+  title: string;
 }
 
 export function ButtonIcon({ title, ...rest } : Props){
-    return(
-        <TouchableOpacity
-            style={styles.container}
-            {...rest}
-        >
-            <View style={styles.iconWrapper}>
-                <Image source={DiscordImg} style={styles.icon} />  
-            </View>
+  return(
+    <TouchableOpacity 
+      style={styles.container} 
+      {...rest }
+    >
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
 
-            <Text style={styles.title}>
-                Entar com discord
-            </Text>
-        </TouchableOpacity>
-    )
+      <Text style={styles.title}>
+        { title }
+      </Text>
+    </TouchableOpacity>
+  );
 }
