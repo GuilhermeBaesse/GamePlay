d7e7bc197d611878a1cfa8441d1a5283ca54a5d3
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-22 08:55:51 -0300

    estilo da interface

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,9 +1,31 @@
 /*regras de estilização da interface*/
-import { StyleSheet } from "react-native";
-
+import { StyleSheet, TouchableHighlightComponent } from "react-native";
+import { theme } from '../../global/styles/theme';
 export const styles = StyleSheet.create({
     container: {
-        felx: 1,
-        backgroundColor: 'red'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.colors.background
+    },
+    image: {
+        width: '100%',
+        height: 360
+    },
+    content: {
+        marginTop: -40,
+        paddingHorizontal: 50,
+    },
+    title: {
+        color: theme.color.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16, 
+    },
+    subtitle: {
+        color: theme.color.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64
     }
 })
