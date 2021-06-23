11ddcb7ba8b832150fb5dcea325033405dc865cf
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-23 13:29:43 -0300

    alterando background

--- src/componets/Background/index.tsx
+++ src/componets/Background/index.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { LinearGradient  } from 'expo-linear-gradient';
 
 import { styles } from './styles'
 import { theme } from '../../global/styles/theme';
-import { ReactNode } from 'react';
+
 
 type Props = {
     children: ReactNode;
 }
 
-export function Background({} : Props) {
+export function Background({ children } : Props) {
     const { secondary80, secondary100} = theme.colors;
     return (
         <LinearGradient  
             style={styles.container}
             colors={[secondary80, secondary100]} 
         >
-
+            {children}
         </LinearGradient>
     )
 }
