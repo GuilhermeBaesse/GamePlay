7d65d4854669e40fb6c8158794237ddf4d8a3a78
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-22 22:41:09 -0300

    adicionando componentes para a categoria

---
+++ src/componets/CategorySelecy/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import {Text, Image, View,} from 'react-native';
+
+
+import { styles } from './styles';
+
+
+export function CategorySelect(){
+  return(
+  
+      <ScrollView
+        horizontal    
+        style={styles.container}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{paddingRight: 40}}
+      >
+        {}
+      </ScrollView>
+
+  
+
+  );
+}
