aedb46a58a230a241e9548b2e40669594477930a
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-29 15:58:05 -0300

    LogBox adicionada

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
-import { useFonts } from 'expo-font';
+import { StatusBar, LogBox } from 'react-native';
 import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
-import { Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
+
+import { AuthProvider } from './src/hooks/auth';
 
 import { Routes } from './src/routes';
-import { Background } from './src/componets/Background'
+import { Background } from './src/componets/Background';
 
 export default function App(){
   const [fontsLoaded] = useFonts({
@@ -17,20 +21,19 @@ export default function App(){
   });
 
   if(!fontsLoaded){
-    /*segura tela de splash para fontes carregarem*/
     return <AppLoading/>
   }
 
   return(
-  <Background> 
-    <StatusBar 
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-    />
-
-    <Routes />
-  </Background>
-  
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Background>
   );
 }
