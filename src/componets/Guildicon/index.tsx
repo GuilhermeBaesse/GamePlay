598d822b37abae5a0503a0325fa09d48a9c32ec4
GuilhermeBaesse <guilherme.baesse@estudante.ufla.br>
2021-06-29 16:00:50 -0300

    alterando compenentes guild para ficarem dinamicos

--- src/componets/Guildicon/index.tsx
+++ src/componets/Guildicon/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image ,View} from 'react-native';
 
 import { styles } from './styles';
+import DiscordSvg from '../../assets/discord.svg';
+const { CDN_IMAGE } = process.env;
 
-export function GuildIcon() {
-  const uri = 'https://gamerssuffice.com/wp-content/uploads/2019/11/How-to-add-bots-to-discord-500x405.jpg';
+type Props = {
+  guildId: string;
+  iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId}: Props) {
+  //pegando imagem do user
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`
+  
 
   return (
-    <Image 
-      source={{ uri }}
-      style={styles.image}
-      resizeMode="cover"    
-    />
-  )
+  <View style={styles.container}>
+      
+    {
+    iconId ?
+      <Image 
+        source={{ uri }}
+        style={styles.image}
+        resizeMode="cover"    
+      /> 
+      :
+      <DiscordSvg width={40} height={40}/>
+    }
+   
+  </View> 
+  )    
 
 }
